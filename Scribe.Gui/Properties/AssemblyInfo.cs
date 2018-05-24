a4c2fdb5bb0d1e81b3dbc15cfc5bb1a160920769
Evgeniy Plyusnin <eninbox@gmail.com>
2018-05-24 17:20:54 +0500

    AssemblyInfo и всё такое

--- Scribe.Gui/Properties/AssemblyInfo.cs
+++ Scribe.Gui/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Scribe.Gui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("The Scribe")]
+[assembly: AssemblyDescription("Программа для сбора и отображения текстово-событийных логов")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("НПО САУТ")]
 [assembly: AssemblyProduct("Scribe.Gui")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
